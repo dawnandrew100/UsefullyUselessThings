d9146718ea768475581705d675a7ee6f6c11c182
Andrew Hennis <81731313+dawnandrew100@users.noreply.github.com>
2023-11-08 21:00:06 +0000

    created js and json files to run using npm run dev

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const PORT = 5000;
+
+
+app.use(express.static("/workspaces/UsefullyUselessThings"));
+
+app.get('/', (req, res) => {
+  res.sendFile("/workspaces/UsefullyUselessThings/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`Development app listening on PORT ${PORT}`);
+});
+
+// put this under app.get to have simple string appear -> res.send('Hello World');
