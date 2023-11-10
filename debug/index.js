beee7948e5236dc8796ec2a34f00bc711814bc3f
Andrew Hennis <81731313+dawnandrew100@users.noreply.github.com>
2023-11-10 17:49:59 +0000

    a

--- debug/index.js
+++ debug/index.js
@@ -3,11 +3,14 @@ import express from 'express';
 const app = express();
 const PORT = 5000;
 
+import path from "path";
 
 app.use(express.static("/workspaces/UsefullyUselessThings"));
+app.use(express.static(path.join(__dirname), "scripts/index.css"));
 
 app.get('/', (req, res) => {
   res.sendFile("/workspaces/UsefullyUselessThings/docs/index.html");
+
 });
 
 app.listen(PORT, () => {
