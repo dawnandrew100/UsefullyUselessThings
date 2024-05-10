0ff27cb30c4a834deee8fc86c36cdecf1214c97f
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-10 11:02:00 -0400

    Changed navbar sticky class to position: sticky

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -5,12 +5,10 @@ let navPos = navbar.getBoundingClientRect().top;
 window.addEventListener("scroll", e => {
   let scrollPos = window.scrollY;
   if (scrollPos > navPos) {
-    navbar.classList.add('sticky');
     navbar.classList.add('transition');
     navbar.classList.remove('endtransition');
     header.classList.add('navbarOffsetMargin');
   } else {
-    navbar.classList.remove('sticky');
     navbar.classList.add('endtransition');
     header.classList.remove('navbarOffsetMargin');
   }
