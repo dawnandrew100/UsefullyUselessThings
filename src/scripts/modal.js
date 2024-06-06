15b38ee71432020a4be32292e02b86227ceeb95e
Andrew Hennis <dawnandrew100@gmail.com>
2024-06-05 21:27:43 -0400

    made close button work for all modals, not just the first span

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -7,43 +7,45 @@ var modal = document.getElementById("BaltimoreCityModal");
 var county = document.getElementById("BaltimoreCity");
 */
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.querySelectorAll(".close");
 
 var modal = "";
 var county = "";
 
 function clicked(clicked_id) {
-    let tempcounty = clicked_id;
-    let tempmodal = clicked_id + "Modal";
-    county = document.getElementById(tempcounty);
-    modal = document.getElementById(tempmodal);
-    openmodal()
+  let tempcounty = clicked_id;
+  let tempmodal = clicked_id + "Modal";
+  county = document.getElementById(tempcounty);
+  modal = document.getElementById(tempmodal);
+  openmodal();
 }
 
 // When the user clicks on the button, open the modal
 function openmodal() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
+span.forEach(function (element) {
+  element.onclick = function () {
+    modal.style.display = "none";
+  };
+});
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 
 //THE FOLLOWING CODE IS FOR THE COLLAPSIBLE MENUS IN THE MODAL WINDOW
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
@@ -51,7 +53,7 @@ for (i = 0; i < coll.length; i++) {
     } else {
       content.style.display = "block";
     }
-    if (content.style.maxHeight){
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
